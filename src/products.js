65cdcaccff4044baa1dea2048b9e3787ad4ec2a7
AkankshaDCedcoss <akankshadwivedi@cedcommerce.com>
2022-02-18 12:20:24 +0530

    done

--- src/products.js
+++ src/products.js
@@ -1,76 +1,155 @@
 var products = [
-  { id: 101, name: "Basket Ball", image: "basketball.png", price: 150 },
-  { id: 102, name: "Football", image: "football.png", price: 120 },
-  { id: 103, name: "Soccer", image: "soccer.png", price: 110 },
-  { id: 104, name: "Table Tennis", image: "table-tennis.png", price: 130 },
-  { id: 105, name: "Tennis", image: "tennis.png", price: 100 },
+  {
+    id: 101,
+    name: "Basket Ball",
+    image: "basketball.png",
+    price: 150,
+    count: 1,
+    totalprice: 150,
+  },
+  {
+    id: 102,
+    name: "Football",
+    image: "football.png",
+    price: 120,
+    count: 1,
+    totalprice: 120,
+  },
+  {
+    id: 103,
+    name: "Soccer",
+    image: "soccer.png",
+    price: 110,
+    count: 1,
+    totalprice: 110,
+  },
+  {
+    id: 104,
+    name: "Table Tennis",
+    image: "table-tennis.png",
+    price: 130,
+    count: 1,
+    totalprice: 130,
+  },
+  {
+    id: 105,
+    name: "Tennis",
+    image: "tennis.png",
+    price: 100,
+    count: 1,
+    totalprice: 100,
+  },
 ];
 var addToCarts = [];
+
 $(document).ready(function () {
   $("#cart1").click(function () {
-    
-
-    for(var i=0;i<products.length;i++)
-    { var count=0;
-      if(products[i].id==101)
-      {count++;
-    addToCarts.push(products[i]);
+    for (var i = 0; i < products.length; i++) {
+      if (products[i].id == 101) {
+        var flag = 0;
+        for (var m = 0; m < addToCarts.length; m++) {
+          if (addToCarts[m].id == 101) {
+            addToCarts[m].count++;
+            flag = 1;
+            break;
+          }
+        }
+        if (flag != 1) {
+          console.log("Inside Flag");
+          addToCarts.push(products[i]);
+        }
       }
     }
-
     display();
   });
   $("#cart2").click(function () {
-
-    for(var i=0;i<products.length;i++)
-    {
-      if(products[i].id==102){
-    addToCarts.push(products[i]);
+    for (var i = 0; i < products.length; i++) {
+      if (products[i].id == 102) {
+        var flag = 0;
+        for (var m = 0; m < addToCarts.length; m++) {
+          if (addToCarts[m].id == 102) {
+            addToCarts[m].count++;
+            flag = 1;
+            break;
+          }
+        }
+        if (flag != 1) {
+          console.log("Inside Flag");
+          addToCarts.push(products[i]);
+        }
+      }
     }
-  }
-
     display();
   });
   $("#cart3").click(function () {
-
-    for(var i=0;i<products.length;i++)
-    { 
-      if(products[i].id==103){
-      addToCarts.push(products[i]);
+    for (var i = 0; i < products.length; i++) {
+      if (products[i].id == 103) {
+        var flag = 0;
+        for (var m = 0; m < addToCarts.length; m++) {
+          if (addToCarts[m].id == 103) {
+            addToCarts[m].count++;
+            flag = 1;
+            break;
+          }
+        }
+        if (flag != 1) {
+          console.log("Inside Flag");
+          addToCarts.push(products[i]);
+        }
       }
     }
 
     display();
   });
   $("#cart4").click(function () {
-
-    for(var i=0;i<products.length;i++)
-    {
-      if(products[i].id==104){
-      addToCarts.push(products[i]);
+    for (var i = 0; i < products.length; i++) {
+      if (products[i].id == 104) {
+        var flag = 0;
+        for (var m = 0; m < addToCarts.length; m++) {
+          if (addToCarts[m].id == 104) {
+            addToCarts[m].count++;
+            flag = 1;
+            break;
+          }
+        }
+        if (flag != 1) {
+          console.log("Inside Flag");
+          addToCarts.push(products[i]);
+        }
       }
     }
 
     display();
   });
   $("#cart5").click(function () {
-
-    for(var i=0;i<products.length;i++)
-    { 
-      if(products[i].id==105){
-      addToCarts.push(products[i]);
+    for (var i = 0; i < products.length; i++) {
+      if (products[i].id == 105) {
+        var flag = 0;
+        for (var m = 0; m < addToCarts.length; m++) {
+          if (addToCarts[m].id == 105) {
+            addToCarts[m].count++;
+            flag = 1;
+            break;
+          }
+        }
+        if (flag != 1) {
+          console.log("Inside Flag");
+          addToCarts.push(products[i]);
+        }
       }
     }
 
     display();
   });
-  
 });
 function add(addToCarts) {
- var count=1;
   html =
-    "<table><tr><th>Id</th><th>Name</th><th>Image</th><th>price</th><th>quantity</th></tr>";
+    "<table><tr><th>Id</th><th>Name</th><th>Image</th><th>price</th><th>quantity</th><th>Totalprice</th></tr>";
   for (var i = 0; i < addToCarts.length; i++) {
+    var count = 1;
+    if (addToCarts[i].id == 101) {
+      console.log(count);
+    }
     html +=
       "<tr><td>" +
       addToCarts[i].id +
@@ -80,17 +159,24 @@ function add(addToCarts) {
       addToCarts[i].image +
       "</td><td>" +
       addToCarts[i].price +
-      "</td><td>"+count+"<button id='increase'>+</button><button id='decrease'>-</button>";
+      "</td><td>" +
+      addToCarts[i].count +
+      "<button id='increase' onclick='increase(" +
+      addToCarts[i].id +
+      ")'>+</button><button id='decrease'  onclick='decrease(" +
+      addToCarts[i].id +
+      ")'>-</button>" +
+      "</td><td>" +
+      addToCarts[i].totalprice +
+      "</td></tr>";
   }
   html += "</table>";
   document.getElementById("output").innerHTML = html;
   console.log(html);
 }
-function display()
-{
-  var count=1;
+function display() {
   html =
-    "<table><tr><th>Id</th><th>Name</th><th>Image</th><th>price</th><th>quantity</th></tr>";
+    "<table><tr><th>Id</th><th>Name</th><th>Image</th><th>price</th><th>quantity</th><th>Totalprice</th></tr>";
   for (var i = 0; i < addToCarts.length; i++) {
     html +=
       "<tr><td>" +
@@ -101,8 +187,48 @@ function display()
       addToCarts[i].image +
       "</td><td>" +
       addToCarts[i].price +
-      "</td><td>"+count+"<button id='increase'>+</button><button id='decrease'>-</button>";
+      "</td><td>" +
+      addToCarts[i].count +
+      "<button id='increase' onclick='increase(" +
+      addToCarts[i].id +
+      ")'>+</button><button id='decrease'  onclick='decrease(" +
+      addToCarts[i].id +
+      ")'>-</button>" +
+      "</td><td>" +
+      addToCarts[i].totalprice +
+      "</td></tr>";
   }
   html += "</table>";
   document.getElementById("output").innerHTML = html;
+}
+function increase(x) {
+  console.log("Inside Increase");
+  for (var m = 0; m < addToCarts.length; m++) {
+    if (addToCarts[m].id == x) {
+      var quant = addToCarts[m].count++;
+      var pr = addToCarts[m].price * addToCarts[m].count;
+
+      console.log(pr);
+
+      addToCarts[m].totalprice = pr;
+    }
+  }
+
+  display();
+}
+function decrease(y) {
+  console.log("Inside Decrease");
+  for (var m = 0; m < addToCarts.length; m++) {
+    if (addToCarts[m].id == y) {
+      var quant = addToCarts[m].count--;
+      console.log(quant);
+      var pr = addToCarts[m].price * addToCarts[m].count;
+
+      console.log(pr);
+
+      addToCarts[m].totalprice = pr;
+    }
+  }
+
+  display();
 }
